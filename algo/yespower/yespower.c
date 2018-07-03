e96864e2ecea247b22d0404f0cfad0fcbc7267d0
bubasik <35274014+bubasik@users.noreply.github.com>
2018-07-03 15:32:47 +0300

    Create yespower.c

---
+++ algo/yespower/yespower.c
@@ -0,0 +1,95 @@
+/*-
+ * Copyright 2018 Cryply team
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file was originally written by Cryply team as part of the Cryply
+ * coin.
+ */
+#include "yespower.h"
+//#include "sysendian.h"
+
+#include "algo-gate-api.h"
+
+static const yespower_params_t v1 = {YESPOWER_0_5, 4096, 16, "Client Key", 10};
+
+static const yespower_params_t v2 = {YESPOWER_0_9, 2048, 32, NULL, 0};
+
+/*
+int yespower_hash(const char *input, char *output)
+{
+    uint32_t time = le32dec(&input[68]);
+    if (time > 1530403200) {
+        return yespower_tls(input, 80, &v2, (yespower_binary_t *) output);
+    } else {
+        return yespower_tls(input, 80, &v1, (yespower_binary_t *) output);
+    }
+};
+*/
+
+void yespower_hash( const char *input, char *output, uint32_t len )
+{
+   yespower_tls( (yespower_binary_t*)input, len, &v2, (yespower_binary_t*)output );
+}
+
+int scanhash_yespower( int thr_id, struct work *work, uint32_t max_nonce,
+                       uint64_t *hashes_done )
+{
+        uint32_t _ALIGN(64) vhash[8];
+        uint32_t _ALIGN(64) endiandata[20];
+        uint32_t *pdata = work->data;
+        uint32_t *ptarget = work->target;
+
+        const uint32_t Htarg = ptarget[7];
+        const uint32_t first_nonce = pdata[19];
+        uint32_t n = first_nonce;
+
+        for (int k = 0; k < 19; k++)
+                be32enc(&endiandata[k], pdata[k]);
+
+        do {
+                be32enc(&endiandata[19], n);
+                yespower_hash((char*) endiandata, (char*) vhash, 80);
+                if (vhash[7] < Htarg && fulltest(vhash, ptarget)) {
+                        work_set_target_ratio( work, vhash );
+                        *hashes_done = n - first_nonce + 1;
+                        pdata[19] = n;
+                        return true;
+                }
+                n++;
+        } while (n < max_nonce && !work_restart[thr_id].restart);
+
+        *hashes_done = n - first_nonce + 1;
+        pdata[19] = n;
+
+        return 0;
+}
+
+bool register_yespower_algo( algo_gate_t* gate )
+{
+  gate->scanhash   = (void*)&scanhash_yespower;
+  gate->hash      = (void*)&yespower_hash;
+  gate->set_target = (void*)&scrypt_set_target;
+  return true;
+};
+
