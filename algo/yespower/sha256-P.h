4f8a3f955fb67ef71103d17e2fdf3877b21230fe
bubasik <35274014+bubasik@users.noreply.github.com>
2018-07-03 15:47:44 +0300

    Update sha256-P.h

--- algo/yespower/sha256-P.h
+++ algo/yespower/sha256-P.h
@@ -111,11 +111,11 @@ void HMAC_SHA256_Final(uint8_t[32], HMAC_SHA256_CTX *);
 void HMAC_SHA256_Buf(const void *, size_t, const void *, size_t, uint8_t[32]);
 
 /**
- * PBKDF2_SHA256(passwd, passwdlen, salt, saltlen, c, buf, dkLen):
+ * PBKDF2_SHA256_P(passwd, passwdlen, salt, saltlen, c, buf, dkLen):
  * Compute PBKDF2(passwd, salt, c, dkLen) using HMAC-SHA256 as the PRF, and
  * write the output to buf.  The value dkLen must be at most 32 * (2^32 - 1).
  */
-void PBKDF2_SHA256(const uint8_t *, size_t, const uint8_t *, size_t,
+void PBKDF2_SHA256_P(const uint8_t *, size_t, const uint8_t *, size_t,
     uint64_t, uint8_t *, size_t);
 
 #endif /* !_SHA256_H_ */
