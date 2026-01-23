5a73568bf0caf0592564d55616a1594ea8206735
Daniel Morante <tuaris@users.noreply.github.com>
2026-01-22 22:04:15 -0600

    Add FreeBSD check for ARM auxiliary vector

--- sysinfos.c
+++ sysinfos.c
@@ -17,7 +17,7 @@
 #include "simd-utils.h"
 
 // hwcap.h missing on MinGW, MacOS
-#if defined(__aarch64__) && !(defined(WIN32) || defined(__APPLE__)) 
+#if defined(__aarch64__) && !(defined(WIN32) || defined(__APPLE__) || defined(__FreeBSD__)) 
 #define ARM_AUXV 
 #endif
 
